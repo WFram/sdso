b44033476a85b8be9b36487d4b1dffba2a48a0b9
WFram <earl.freedom.ea@gmail.com>
2023-02-25 19:00:11 +0300

    Fix path to cv_bridge_local

--- cmake/FindOpenCVLocal.cmake
+++ cmake/FindOpenCVLocal.cmake
@@ -1,58 +1,58 @@
 FIND_PATH(opencv_INCLUDE_DIR
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/include/opencv4/opencv2
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/include/opencv4/opencv2
         )
 
 message("opencv_INCLUDE_DIR: ${opencv_INCLUDE_DIR}")
 
 FIND_LIBRARY(opencv_core_LIB NAMES opencv_core
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/lib
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/lib
         )
 
 FIND_LIBRARY(opencv_flann_LIB NAMES opencv_flann
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/lib
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/lib
         )
 
 FIND_LIBRARY(opencv_imgproc_LIB NAMES opencv_imgproc
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/lib
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/lib
         )
 
 FIND_LIBRARY(opencv_features2d_LIB NAMES opencv_features2d
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/lib
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/lib
         )
 
 FIND_LIBRARY(opencv_imgcodecs_LIB NAMES opencv_imgcodecs
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/lib
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/lib
         )
 
 FIND_LIBRARY(opencv_videoio_LIB NAMES opencv_videoio
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/lib
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/lib
         )
 
 FIND_LIBRARY(opencv_calib3d_LIB NAMES opencv_calib3d
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/lib
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/lib
         )
 
 FIND_LIBRARY(opencv_highgui_LIB NAMES opencv_highgui
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/lib
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/lib
         )
 
 FIND_LIBRARY(opencv_video_LIB NAMES opencv_video
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/lib
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/lib
         )
 
 FIND_LIBRARY(opencv_xfeatures2d_LIB NAMES opencv_xfeatures2d
         PATHS
-        ${CMAKE_BINARY_DIR}/../cv_bridge/3rd_party/src/opencv_external-build/install/lib
+        ${CMAKE_BINARY_DIR}/../cv_bridge_local/3rd_party/src/opencv_external-build/install/lib
         )
 
 SET(OPENCV_LIBS

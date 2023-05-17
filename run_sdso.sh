25ffe715c0a19beda828600d77352dbcbda5e6dc
WFram <earl.freedom.ea@gmail.com>
2023-05-17 03:08:32 +0300

    Add external opencv

--- run_sdso.sh
+++ run_sdso.sh
@@ -1,9 +1,10 @@
-SEQ="$1"
-./build/bin/dso_dataset \
-    files=/media/wfram/Samsung_data1/zed2_dso/dog_stereo_sequence \
-    calib=/media/wfram/Samsung_data1/zed2_dso/camera_zed2_radtan.txt \
-    mode=0 \
+SEQ=$1
+# gdb -ex run --args
+./build/bin/sdso_dataset \
+    files=$SEQ \
+    calib=$SEQ/stereo_camera.txt \
+    mode=1 \
     nogui=0 \
     quiet=1 \
-    start=0
-    # start=0
+    start=500 \
+    end=10000
